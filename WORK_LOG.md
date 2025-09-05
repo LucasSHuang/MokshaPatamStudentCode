27e4e80b4d0220816e3d41553494052071a5e7ac
Lucas Huang <lucas.huang@menloschool.org>
2025-09-04 20:06:14 -0700

    Created queue for BFS

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,10 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date  |   Time    |                                                                                                                    Update |
+|:------|:---------:|--------------------------------------------------------------------------------------------------------------------------:|
+| Sep 3 | 2:30-3pm  |                    I did a lot of brainstorming and got to the point of solving the problem without any snakes or ladders |
+| Sep 4 | 4-4:30pm  |                     More brainstorming, contemplating the BFS method for this, but don't exactly know how to implement it |
+| Sep 4 | 6:45-7:30 | I implemented all of the snakes, ladders, and normal spots into a array, hopefully from this I can create a queue for BFS |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -1,21 +1,50 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Moksha Patam
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Lucas Huang
  *
  */
 
 public class MokshaPatam {
 
+    final static int MAX_MOVE = 6;
+    final static int START = 1;
     /**
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
+        int[] board = new int[boardsize + 1];
+        for (int i = 1; i < board.length; i++) {
+            if (ladders[i] != null) {
+                board[i] = ladders[i][0];
+            }
+            else if (snakes[i] != null) {
+                board[i] = snakes[i][0];
+            }
+            else {
+                board[i] = i;
+            }
+        }
+        Queue<Integer> solution = new LinkedList<Integer>();
+        int current = START;
 
+        int playerPostition = boardsize;
+        int count = 0;
+        while (playerPostition != START) {
+            if (playerPostition <= MAX_MOVE) {
+                return count + 1;
+            }
+            else {
+                playerPostition -= MAX_MOVE;
+            }
+        }
         return 0;
     }
 }
