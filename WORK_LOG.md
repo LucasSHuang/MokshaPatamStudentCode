bd5fa7cd369db63d843e576b6d1073799dda2396
Lucas Huang <lucas.huang@menloschool.org>
2025-09-07 09:44:37 -0700

    Finished a lot of bfs

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,10 +1,12 @@
 Name: YOUR NAME HERE
 
-| Date  |   Time    |                                                                                                                    Update |
-|:------|:---------:|--------------------------------------------------------------------------------------------------------------------------:|
-| Sep 3 | 2:30-3pm  |                    I did a lot of brainstorming and got to the point of solving the problem without any snakes or ladders |
-| Sep 4 | 4-4:30pm  |                     More brainstorming, contemplating the BFS method for this, but don't exactly know how to implement it |
-| Sep 4 | 6:45-7:30 | I implemented all of the snakes, ladders, and normal spots into a array, hopefully from this I can create a queue for BFS |
+| Date  |    Time     |                                                                                                                                           Update |
+|:------|:-----------:|-------------------------------------------------------------------------------------------------------------------------------------------------:|
+| Sep 3 |  2:30-3pm   |                                           I did a lot of brainstorming and got to the point of solving the problem without any snakes or ladders |
+| Sep 4 |  4-4:30pm   |                                            More brainstorming, contemplating the BFS method for this, but don't exactly know how to implement it |
+| Sep 4 |  6:45-7:30  |                        I implemented all of the snakes, ladders, and normal spots into a array, hopefully from this I can create a queue for BFS |
+| Sep 6 | 11:45-12am  |                                              I implemented more of the BFS, trying to do the same thing as MazeSolver but its a little different |
+| Sep 7 | 9:15-9:45am | I thought I finished the BFS but I think the way I am counting the amount of values is wrong, maybe I should try doing an array to count instead |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -15,36 +15,52 @@ public class MokshaPatam {
 
     final static int MAX_MOVE = 6;
     final static int START = 1;
+
+    private static int[] board;
     /**
      * TODO: Complete this function, fewestMoves(), to return the minimum number of moves
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        int[] board = new int[boardsize + 1];
+        board = new int[boardsize + 1];
         for (int i = 1; i < board.length; i++) {
-            if (ladders[i] != null) {
-                board[i] = ladders[i][0];
-            }
-            else if (snakes[i] != null) {
-                board[i] = snakes[i][0];
-            }
-            else {
                 board[i] = i;
-            }
         }
-        Queue<Integer> solution = new LinkedList<Integer>();
+        addThings(ladders);
+        addThings(snakes);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        boolean[] visited = new boolean[boardsize + 1];
         int current = START;
-
-        int playerPostition = boardsize;
+        queue.add(current);
+        visited[START] = true;
         int count = 0;
-        while (playerPostition != START) {
-            if (playerPostition <= MAX_MOVE) {
-                return count + 1;
+        while (!queue.isEmpty()) {
+            current = queue.remove();
+            if (current == boardsize) {
+                return count;
             }
             else {
-                playerPostition -= MAX_MOVE;
+                for (int i = 1; i <= MAX_MOVE; i++) {
+                    int next = current + i;
+                    if (next > boardsize) {
+                        continue;
+                    }
+                    next = board[next];
+                    if (!visited[next]) {
+                        queue.add(board[current + i]);
+                        visited[next] = true;
+                    }
+                }
             }
+            count++;
         }
-        return 0;
+        return -1;
+    }
+    public static void addThings(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            int start = array[i][0];
+            int end = array[i][1];
+            board[start] = end;
+        }
     }
 }
